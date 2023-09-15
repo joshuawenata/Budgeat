5d349d195e4e303f929d33a44f30998806d408ce
joshuawenata <joshuawenata@gmail.com>
2023-09-15 17:41:25 +0700

    home page for merchant

---
+++ app/src/main/java/com/example/myapplication/HomeMerchant.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class HomeMerchant : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_merchant)
+    }
+}
