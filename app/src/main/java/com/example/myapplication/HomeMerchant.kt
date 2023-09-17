e127dde391f802e5a2919f2b0f05384b29736ef8
joshuawenata <joshuawenata@gmail.com>
2023-09-17 22:15:34 +0700

    add stock

--- app/src/main/java/com/example/myapplication/HomeMerchant.kt
+++ app/src/main/java/com/example/myapplication/HomeMerchant.kt
@@ -1,20 +1,14 @@
 package com.example.myapplication
 
-import Adapter
+import com.example.myapplication.adapter.AdapterAddStock
 import android.annotation.SuppressLint
-import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.`object`.Menu
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 
 class HomeMerchant : ComponentActivity() {
     @SuppressLint("NotifyDataSetChanged")
@@ -26,22 +20,33 @@ class HomeMerchant : ComponentActivity() {
             // Now you can use the menuList or pass it to your RecyclerView setup code
             val recyclerView = findViewById<RecyclerView>(R.id.menu_recyclerview)
             recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-            val newAdapter = Adapter(
+            val newAdapter = AdapterAddStock(
                 this,
                 menuList,
-                R.layout.card_menu
-            ) { itemView, item ->
-                val menuNameTextView = itemView.findViewById<TextView>(R.id.card_menu_name)
-                val menuDescriptionTextView = itemView.findViewById<TextView>(R.id.card_menu_description)
-                val menuStockTextView = itemView.findViewById<TextView>(R.id.card_menu_stock)
+                R.layout.card_menu,
+                { itemView, item ->
+                    val menuNameTextView = itemView.findViewById<TextView>(R.id.card_menu_name)
+                    val menuDescriptionTextView = itemView.findViewById<TextView>(R.id.card_menu_description)
+                    val menuStockTextView = itemView.findViewById<TextView>(R.id.card_menu_stock)
 
-                menuNameTextView.text = item.menuName
-                menuDescriptionTextView.text = item.menuDescription
-                menuStockTextView.text = item.menuStock
-            }
+                    menuNameTextView.text = item.menuName
+                    menuDescriptionTextView.text = item.menuDescription
+                    menuStockTextView.text = item.menuStock
+                },
+                { item ->
+                    // Handle item click here
+                },
+                { item ->
+                    // Handle button click here
+                    val intent = Intent(this, AddStock::class.java)
+                    intent.putExtra("menuKey",item.menuKey)
+                    startActivity(intent)
+                }
+            )
 
             recyclerView.adapter = newAdapter
         }
+
 
     }
 
---
+++ app/src/main/java/com/example/myapplication/adapter/AdapterAddStock.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+
+class AdapterAddStock<T>(
+    private val context: Context,
+    private val items: List<T>,
+    private val itemLayoutResId: Int,
+    private val bindView: (View, T) -> Unit,
+    private val onItemClick: (T) -> Unit,
+    private val onItemButtonClickListener: (T) -> Unit
+) : RecyclerView.Adapter<AdapterAddStock<T>.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: T) {
+            bindView(itemView, item)
+
+            // Set an onClickListener for the item view
+            itemView.setOnClickListener {
+                onItemClick(item)
+            }
+
+            // Set an onClickListener for the button within the item view
+            val button = itemView.findViewById<ImageButton>(R.id.card_add_stock) // Replace with your button ID
+            button.setOnClickListener {
+                onItemButtonClickListener(item)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(itemLayoutResId, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
